e18d4b40300db19f236c62637909317c43d9f65d
Mathieu Baudonnat <m.baudonnat@yahoo.fr>
2016-02-18 10:33:29 +0100

    Implementation de barre laterale de DL sur custom type + metabox

--- functions.php
+++ functions.php
@@ -252,3 +252,236 @@ add_action('wp_enqueue_scripts', 'bones_fonts');
 /* DON'T DELETE THIS CLOSING TAG */ ?>
 
 
+<?php
+/**
+ * Include and setup custom metaboxes and fields. (make sure you copy this file to outside the CMB2 directory)
+ *
+ * Be sure to replace all instances of 'le_barp_' with your project's prefix.
+ * http://nacin.com/2010/05/11/in-wordpress-prefix-everything/
+ *
+ * @category le_barp
+ * @package  Demo_CMB2
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     https://github.com/WebDevStudios/CMB2
+ */
+
+/**
+ * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
+ */
+
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+  require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+  require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+
+/**
+ * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
+ *
+ * @param  CMB2 object $cmb CMB2 object
+ *
+ * @return bool             True if metabox should show
+ */
+function le_barp_show_if_front_page( $cmb ) {
+  // Don't show this metabox if it's not the front page template
+  if ( $cmb->object_id !== get_option( 'page_on_front' ) ) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
+ *
+ * @param  CMB2_Field object $field Field object
+ *
+ * @return bool                     True if metabox should show
+ */
+function le_barp_hide_if_no_cats( $field ) {
+  // Don't show this field if not in the cats category
+  if ( ! has_tag( 'cats', $field->object_id ) ) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Conditionally displays a message if the $post_id is 2
+ *
+ * @param  array             $field_args Array of field parameters
+ * @param  CMB2_Field object $field      Field object
+ */
+function le_barp_before_row_if_2( $field_args, $field ) {
+  if ( 2 == $field->object_id ) {
+    echo '<p>Testing <b>"before_row"</b> parameter (on $post_id 2)</p>';
+  } else {
+    echo '<p>Testing <b>"before_row"</b> parameter (<b>NOT</b> on $post_id 2)</p>';
+  }
+}
+
+
+add_action( 'cmb2_admin_init', 'le_barp_register_repeatable_group_field_metabox' );
+/**
+ * Hook in and add a metabox to demonstrate repeatable grouped fields
+ */
+function le_barp_register_repeatable_group_field_metabox() {
+
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_le_barp_group_';
+
+  /**
+   * Repeatable Field Groups
+   */
+  $cmb_group = new_cmb2_box( array(
+    'id'           => $prefix . 'metabox',
+    'title'        => __( 'Repeating Field Group', 'cmb2' ),
+    'object_types' => array( 'custom_type', ),
+  ) );
+
+  // $group_field_id is the field id string, so in this case: $prefix . 'demo'
+  $group_field_id = $cmb_group->add_field( array(
+    'id'          => $prefix . 'demo',
+    'type'        => 'group',
+    'description' => __( 'Generates reusable form entries', 'cmb2' ),
+    'options'     => array(
+      'group_title'   => __( 'Entry {#}', 'cmb2' ), // {#} gets replaced by row number
+      'add_button'    => __( 'Add Another Entry', 'cmb2' ),
+      'remove_button' => __( 'Remove Entry', 'cmb2' ),
+      'sortable'      => true, // beta
+      // 'closed'     => true, // true to have the groups closed by default
+    ),
+    'fields'      => array(
+                        array(
+          'name' => __( 'Title', 'cmb2' ),
+              'id'   => 'title',
+              'type' => 'text'
+          ),
+        array(
+          'name' => __( 'Description', 'cmb2' ),
+              'id'   => 'description',
+              'type' => 'textarea_small'
+        ),
+        array(
+          'name' => __( 'Fichier à télécharger', 'cmb2' ),
+          'id'   => 'file_link',
+          'type' => 'file',
+          
+            ),
+      ),
+  ) );
+  
+  
+
+}
+
+add_action( 'cmb2_admin_init', 'le_barp_register_user_profile_metabox' );
+/**
+ * Hook in and add a metabox to add fields to the user profile pages
+ */
+function le_barp_register_user_profile_metabox() {
+
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_le_barp_user_';
+
+  /**
+   * Metabox for the user profile screen
+   */
+  $cmb_user = new_cmb2_box( array(
+    'id'               => $prefix . 'edit',
+    'title'            => __( 'User Profile Metabox', 'cmb2' ),
+    'object_types'     => array( 'user' ), // Tells CMB2 to use user_meta vs post_meta
+    'show_names'       => true,
+    'new_user_section' => 'add-new-user', // where form will show on new user page. 'add-existing-user' is only other valid option.
+  ) );
+
+  $cmb_user->add_field( array(
+    'name'     => __( 'Extra Info', 'cmb2' ),
+    'desc'     => __( 'field description (optional)', 'cmb2' ),
+    'id'       => $prefix . 'extra_info',
+    'type'     => 'title',
+    'on_front' => false,
+  ) );
+
+  $cmb_user->add_field( array(
+    'name'    => __( 'Avatar', 'cmb2' ),
+    'desc'    => __( 'field description (optional)', 'cmb2' ),
+    'id'      => $prefix . 'avatar',
+    'type'    => 'file',
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => __( 'Facebook URL', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'facebookurl',
+    'type' => 'text_url',
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => __( 'Twitter URL', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'twitterurl',
+    'type' => 'text_url',
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => __( 'Google+ URL', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'googleplusurl',
+    'type' => 'text_url',
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => __( 'Linkedin URL', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'linkedinurl',
+    'type' => 'text_url',
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => __( 'User Field', 'cmb2' ),
+    'desc' => __( 'field description (optional)', 'cmb2' ),
+    'id'   => $prefix . 'user_text_field',
+    'type' => 'text',
+  ) );
+
+}
+
+add_action( 'cmb2_admin_init', 'le_barp_register_theme_options_metabox' );
+/**
+ * Hook in and register a metabox to handle a theme options page
+ */
+function le_barp_register_theme_options_metabox() {
+
+  // Start with an underscore to hide fields from custom fields list
+  $option_key = '_le_barp_theme_options';
+
+  /**
+   * Metabox for an options page. Will not be added automatically, but needs to be called with
+   * the `cmb2_metabox_form` helper function. See wiki for more info.
+   */
+  $cmb_options = new_cmb2_box( array(
+    'id'      => $option_key . 'page',
+    'title'   => __( 'Theme Options Metabox', 'cmb2' ),
+    'hookup'  => false, // Do not need the normal user/post hookup
+    'show_on' => array(
+      // These are important, don't remove
+      'key'   => 'options-page',
+      'value' => array( $option_key )
+    ),
+  ) );
+
+  /**
+   * Options fields ids only need
+   * to be unique within this option group.
+   * Prefix is not needed.
+   */
+  $cmb_options->add_field( array(
+    'name'    => __( 'Site Background Color', 'cmb2' ),
+    'desc'    => __( 'field description (optional)', 'cmb2' ),
+    'id'      => 'bg_color',
+    'type'    => 'colorpicker',
+    'default' => '#ffffff',
+  ) );
+
+}
+
